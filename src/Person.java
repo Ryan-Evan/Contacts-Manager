d84b14948e3516b97a79af0c067ae57018e68008
Evan Mead <evan.c.mead@gmail.com>
2020-12-18 15:32:19 -0600

    person created - along with getters and setters

---
+++ src/Person.java
@@ -0,0 +1,20 @@
+public class Person {
+    String name;
+    double phoneNumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(double phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
